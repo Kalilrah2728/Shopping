96b807e710230a2fc240a4f1ace4048da8957291
Kalil Rahman <kalilrahman@twinkletech.com>
2023-01-23 18:32:23 +0530

    Commit on 23-01-2023

--- app/src/main/java/com/kalil/vajrotask/productlist/Retrofit/ApiInterface.kt
+++ app/src/main/java/com/kalil/vajrotask/productlist/Retrofit/ApiInterface.kt
@@ -1,13 +1,7 @@
 package com.kalil.vajrotask.productlist.Retrofit
 
-import androidx.lifecycle.MutableLiveData
 import com.kalil.vajrotask.productlist.model.Datamodel
-import com.kalil.vajrotask.productlist.model.RequestmodelDummy
-import com.twinkle.ncsl.projectutils.Variable
 import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
 interface ApiInterface {
@@ -15,6 +9,6 @@ interface ApiInterface {
     /*@GET("mocky.io")
     fun doGetProducts(@Body body: RequestmodelDummy): Call<Datamodel>*/
 
-    @GET("mocky.io")
+    @POST("mocky.io/v2")
     fun getProduct() : Call<Datamodel>
 }
